a3a22699fcbf96f9ba169fa38b894d7d994ea78b
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-09-04 18:20:09 +0200

    update: add listMailBoxes() method to Worker class

--- server/src/lib/IMAP.ts
+++ server/src/lib/IMAP.ts
@@ -3,6 +3,17 @@ import { IServerInfo } from "../utils/ServerInfo";
 
 export interface IImapClient extends Record<string, any> {}
 
+export interface IMailboxes {
+  root: boolean;
+  children: IMailbox[];
+}
+
+export interface IMailbox {
+  name: string;
+  path: string;
+  children?: IMailbox[];
+}
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export class Worker {
@@ -26,7 +37,31 @@ export class Worker {
     };
 
     await client.connect();
-    
+
     return client;
+  }
+
+  public async listMailboxes(): Promise<IMailbox[]> {
+    const client: IImapClient = await this.connectToServer();
+    const mailboxes: IMailboxes = await client.listMailboxes();
+
+    await client.close();
+
+    const finalMailboxes: IMailbox[] = [];
+
+    const iterateChildren = (children: IMailbox[]): void => {
+      children.forEach((child: IMailbox) => {
+        finalMailboxes.push({
+          name: child.name,
+          path: child.path,
+        });
+
+        child.children && iterateChildren(child.children);
+      });
+    };
+
+    iterateChildren(mailboxes.children);
+
+    return finalMailboxes;
   }
 }
