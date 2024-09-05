3d40b379e09529a03665725289cf5352ee66aef2
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-09-05 22:53:38 +0200

    add & configure project dependencies

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from './routeTree.gen';
 
-createRoot(document.getElementById("root")!).render(
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("root")!;
+
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
