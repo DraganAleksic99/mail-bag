91de61fc18bd15f43f0e8f13138bc620603c639d
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-09-11 00:13:26 +0200

    update (client): add mailbox-skeleton component

---
+++ client/src/components/mailbox-skeleton.tsx
@@ -0,0 +1,43 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Avatar } from "@/components/ui/avatar";
+import { Skeleton } from "./ui/skeleton";
+
+export function MailboxSkeleton() {
+  return (
+    <div className="max-h-screen w-full max-w-2xl mx-auto bg-background border rounded-lg shadow-sm">
+      <div className="p-4 border-b">
+        <Skeleton className="w-fit">
+          <h2 className="text-2xl font-bold text-transparent">xxxxxxx</h2>
+        </Skeleton>
+      </div>
+      <ScrollArea className="h-[calc(100vh-66px)]">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <div
+            key={i}
+            className="flex items-center gap-4 p-4 border-b transition-colors"
+          >
+            <Skeleton className="rounded-full">
+              <Avatar className="flex-shrink-0"></Avatar>
+            </Skeleton>
+            <div className="flex-grow min-w-0">
+              <div className="flex items-center justify-between mb-1">
+                <Skeleton className="w-full">
+                  <span className="font-semibold truncate text-transparent">
+                    xxxxx xxxxxxxx
+                  </span>
+                </Skeleton>
+              </div>
+              <div className="text-sm font-medium mb-1 truncate text-transparent">
+                <Skeleton className="max-w-fit">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste
+                  natus assumenda velit, modi tempora voluptatem repellat illum
+                  nemo iure eaque!
+                </Skeleton>
+              </div>
+            </div>
+          </div>
+        ))}
+      </ScrollArea>
+    </div>
+  );
+}
