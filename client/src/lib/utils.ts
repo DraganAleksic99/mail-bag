b629aae2a9172710869c331ff928b3b8e4bc1f0a
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-09-11 22:02:01 +0200

    update (client): add mapMailboxesToIcons() utility

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,10 +1,109 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import type { Mailbox } from "@/routes/__root";
+import {
+  Inbox,
+  Send,
+  File,
+  Trash2,
+  Archive,
+  Star,
+  AlertCircle,
+  Paperclip,
+  Mail,
+} from "lucide-react";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function capitalizeString([firstLetter, ...rest]: string) {
   return firstLetter.toUpperCase() + rest.join("");
+}
+
+export function mapMailboxesToIcons(mailboxes: Mailbox[]) {
+  const arr = [];
+
+  for (const { name, path } of mailboxes) {
+    switch (path) {
+      case "inbox": {
+        arr.push({
+          name,
+          path,
+          icon: Inbox,
+        })
+      }
+        break;
+      case "drafts": 
+      break;
+      case "saved": {
+        arr.push({
+          name,
+          path,
+          icon: Archive,
+        })
+      }
+      break;
+      case "[gmail]/drafts": {
+        arr.push({
+          name,
+          path,
+          icon: File,
+        })
+      }
+      break;
+      case "[gmail]/all mail": {
+        arr.push({
+          name,
+          path,
+          icon: Mail,
+        })
+      }
+      break;
+      case "[gmail]/bin": {
+        arr.push({
+          name,
+          path,
+          icon: Trash2,
+        })
+      }
+      break;
+      case "[gmail]/important": {
+        arr.push({
+          name,
+          path,
+          icon: Paperclip,
+        })
+      }
+      break;
+      case "[gmail]/sent mail": {
+        arr.push({
+          name,
+          path,
+          icon: Send,
+        })
+      }
+      break;
+      case "[gmail]/spam": {
+        arr.push({
+          name,
+          path,
+          icon: AlertCircle,
+        })
+      }
+      break;
+      case "[gmail]/starred": {
+        arr.push({
+          name,
+          path,
+          icon: Star,
+        })
+      }
+      break;
+      default:
+        break;
+    }
+  }
+
+  return arr;
 }
