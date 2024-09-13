5535d102012896549ea296346a4ebc221d50ee4b
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-09-13 12:13:09 +0200

    update (client): update base-layout component

--- client/src/components/base-layout.tsx
+++ client/src/components/base-layout.tsx
@@ -1,36 +1,22 @@
-import { Outlet, Link } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { Button } from "@/components/ui/button";
-import { Route } from "@/routes/__root";
+import { MailboxesSidebar } from "./mailboxes-sidebar";
+import { Route } from "@/routes/__root";;
 
 export function BaseLayout() {
   const data = Route.useLoaderData();
 
   return (
     <>
-      <nav className="p-4 pb-0 w-full">
-        <ul className="flex gap-2">
-          <li>
-            <Link to="/" className="">
-              <Button>New Message</Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/" className="">
-              <Button>New Contact</Button>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main className="p-4 pb-0">
-        <div className="min-h-[calc(100vh-68px)] h-[calc(100vh-68px)] flex gap-3">
-          <div className="w-1/5 min-h-full overflow-y-auto border-r-2 border-black flex flex-col">
-            <h1>Hi!</h1>
+      <main>
+        <div className="min-h-screen] h-screen flex gap-3">
+          <div className="w-1/5 min-h-full flex flex-col">
+            <MailboxesSidebar mailboxes={data} />
           </div>
           <div className="w-1/2 min-h-full">
             <Outlet />
           </div>
-          <div className="w-[30%] min-h-full overflow-y-auto border-l-2 border-black flex flex-col">
+          <div className="w-[30%] min-h-full overflow-y-auto flex flex-col">
             <h1>Hi!</h1>
           </div>
         </div>
