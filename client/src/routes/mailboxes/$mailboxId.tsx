bf9dd37e6fac08df195921e34e3dc6ba90966fc4
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-09-19 21:42:12 +0200

    update (client): add route for every mailbox

---
+++ client/src/routes/mailboxes/$mailboxId.tsx
@@ -0,0 +1,36 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Mailbox } from "@/components/mailbox";
+import { MailboxSkeleton } from "@/components/mailbox-skeleton";
+
+export type TEmail = {
+  id: string;
+  from: string;
+  subject: string;
+  date: string;
+}
+
+export const Route = createFileRoute("/mailboxes/$mailboxId")({
+  loader: async ({ params: { mailboxId }, abortController }) => {
+
+    const response = await fetch(`http://localhost:80/mailboxes/${mailboxId}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      signal: abortController.signal,
+    });
+
+    const data: TEmail[] = await response.json();
+
+    return {
+      data: data.reverse(),
+      mailboxId,
+    };
+  },
+  staleTime: Infinity,
+  pendingMs: 500,
+  wrapInSuspense: true,
+  component: Mailbox,
+  pendingComponent: MailboxSkeleton,
+});
